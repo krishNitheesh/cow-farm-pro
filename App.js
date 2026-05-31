e60eeee2fabce0aacddab3dd104ab1635011cd21
krishNitheesh <nitheesh8797@gmail.com>
2026-05-31 15:57:55 +0530

    chore: trigger fresh GitHub Actions APK build with entry point documentation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// COW FARM - Mobile Application Entry Point
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
